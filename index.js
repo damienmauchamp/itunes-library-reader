801e02539a9f8d16a79b40c06ff83bd654e776b7
Damien Mauchamp <damienmauchamp@gmail.com>
2019-07-31 16:11:41 +0200

    tests

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ var path = require('path')
 var fs = require('fs');
 var plist = require('plist');
 
+var renameKeys = (obj) => {
+	var keysMap = {};
+	Object.keys(obj).forEach(k => {
+		keysMap[k] = k.toLowerCase().replace(/ /g,"_");
+	});
+	return Object
+		.keys(obj)
+		.reduce((acc, key) => {
+			const renamedObject = { [keysMap[key] || key]: obj[key] };
+			return {
+				...acc,
+				...renamedObject
+			  }
+		}, {});
+};
+
 //var location = path.resolve('./iTunes Music Library.xml')
 var location = path.resolve('./sample.xml')
 //var location = path.resolve('./sample.xml')
@@ -24,16 +40,56 @@ var playlist = playlists.find(p => {
 	return p.Name === playlistName
 })
 if (playlist) {
-	var items = playlist['Playlist Items']
+	var items = playlist['Playlist Items'].map(i => {
+		return renameKeys(i);
+	})
 	var playlistTracks = [];
-
 	items.forEach(track => {
-		if (tracks[track['Track ID']]) {
+		if (tracks[track.track_id]) {
+			track_item = renameKeys(tracks[track.track_id]);
 			// itunes api call
-			// .Year, ['Release Date'], .Name, .Artist, .Album, ['Album Artist']
-			playlistTracks.push(tracks[track['Track ID']]);
+			// .year, .release_date, .name, .artist, .album, .album_artist, explicit
+			playlistTracks.push(track_item);
 		}
 	})
 	//console.log(JSON.stringify(playlists));
 	console.log(playlistTracks);
 }
+
+/*
+
+[ { track_id: 58213,
+    size: 7602465,
+    total_time: 215024,
+    disc_number: 1,
+    disc_count: 1,
+    track_number: 1,
+    track_count: 1,
+    year: 2019,
+    date_modified: 2019-06-13T19:46:14.000Z,
+    date_added: 2019-06-13T19:46:14.000Z,
+    bit_rate: 256,
+    sample_rate: 44100,
+    part_of_gapless_album: true,
+    release_date: 2019-06-13T07:00:00.000Z,
+    artwork_count: 1,
+    persistent_id: '777DDC39BF7C04DD',
+    explicit: true,
+    track_type: 'Remote',
+    apple_music: true,
+    name: 'Ol\' Dirty Bastard',
+    artist: 'B.o.B',
+    album_artist: 'B.o.B',
+    composer: 'Bobby Ray Simmons, Jr.',
+    album: 'Ol\' Dirty Bastard - Single',
+    genre: 'Hip-hop/Rap',
+    kind: 'Fichier audio AAC Apple Music',
+    sort_name: 'Ol\' Dirty Bastard',
+    sort_album: 'Ol\' Dirty Bastard - Single',
+    sort_artist: 'B.o.B' } ]
+
+// singles
+https://itunes.apple.com/search?term=B.o.B Ol' Dirty Bastard Ol' Dirty Bastard - Single&country=fr&explicit=Yes&entity=song&callback=
+// albums
+https://itunes.apple.com/search?term=B.o.B Southmatic&country=fr&explicit=Yes&entity=album&callback=
+*/
